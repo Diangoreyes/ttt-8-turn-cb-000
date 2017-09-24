997918c1a67a44f2b3044dbf36a31d3f0e8935a4
Diango Reyes <ditzun@gmail.com>
2017-09-24 21:56:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def position_taken?(board, index)
 end
 # ----------------------------------------
 def turn(board)
-  puts "Please eneter 1-9:"
+  print "Please eneter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
